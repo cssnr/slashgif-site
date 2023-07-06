5a9b968ef892a425d075039c2af552920cc1bf50
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2023-07-05 17:40:36 -0700

    Update (#4)
    * Minor Fix

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
-if [ "${*:0:3}" = "gun" ];then
-  python manage.py collectstatic --noinput
+if echo "${*}" | grep -q "gun";then
+    if [ -n "${DJANGO_SUPERUSER_PASSWORD}" ] &&
+    [ -n "${DJANGO_SUPERUSER_USERNAME}" ] &&
+    [ -n "${DJANGO_SUPERUSER_EMAIL}" ];then
+        python manage.py createsuperuser --noinput || :
+    fi
+    python manage.py collectstatic --noinput
 fi
 
 exec "$@"
