4033945411b729662e804c9158f530c08f3d4978
Shane <6071159+smashedr@users.noreply.github.com>
2026-07-19 13:37:24 -0700

    Fix docker-entrypoint.sh

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -8,7 +8,7 @@ if echo "${*}" | grep -q "gun\|runserver";then
     [ -n "${DJANGO_SUPERUSER_EMAIL}" ];then
         uv run python manage.py createsuperuser --noinput || :
     fi
-    uv run python manage.py migrate --noinput
+    #uv run python manage.py migrate --noinput
     uv run python manage.py collectstatic --noinput -v 0
 fi
 
